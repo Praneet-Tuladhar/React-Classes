2eccf2cda3e8714e208780f94710350def4edf9e
Praneet-Tuladhar <praneet.tuladhar9679@gmail.com>
2024-07-30 00:28:07 +0545

    edit 4

---
+++ todoList/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+  function deleteTask(index) {
+    const updatedTask = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTask);
+  }
+  return (
+    <>
+      <div className="to-do-list">
+        <h1>To-Do-List</h1>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Enter a task..."
+            value={newTask}
+            onChange={handleInputChange}
+          />
+          <button className="add-button" onClick={addTask}>
+            Add
+          </button>
+        </div>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+}
+
+export default App;
